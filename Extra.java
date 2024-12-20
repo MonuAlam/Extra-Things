f3c5c002e11350c3d320293bb2585e8f2583f6ed
Monu.Alam <132586354+MonuAlam@users.noreply.github.com>
2024-12-20 21:58:07 +0530

    Update Extra.java

--- Extra.java
+++ Extra.java
@@ -3,7 +3,7 @@
 private ProjectEstimation updateWithBuilder(ProjectEstimation estimation, ProjectEstimationRequest request) {
 
     return estimation.toBuilder()
-            .title(request.getTitle() != null ? request.getTitle() : estimation.getTitle())
+            .title(request.getTitle() != null?request.getTitle() : estimation.getTitle())
             .project(request.getProject() != null ? request.getProject() : estimation.getProject())
             .phase(request.getPhase() != null ? request.getPhase() : estimation.getPhase())
             .mileStones(request.getMileStones() != null ? request.getMileStones().stream().map(mile -> {
